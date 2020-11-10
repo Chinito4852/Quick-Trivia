3ff4ceb3a2ff3e35bd46e585f9cd0f215fe41256
Chinito4852 <43899219+Chinito4852@users.noreply.github.com>
2020-11-09 17:36:37 -0800

    Add files via upload

--- end.js
+++ end.js
@@ -6,7 +6,7 @@ const endScore = localStorage.getItem("endScore");
 let scores = localStorage.getItem("scores");
 if (scores === "") scores = [];
 else scores = JSON.parse(scores);
-
+console.log(scores);
 const MAX_SCORES = 10;
 
 // Display score
